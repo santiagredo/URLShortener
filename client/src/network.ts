2d2db8481ee55bd7786ca5829118bbb74b238d37
Santiago S <santiagoagredo@live.com>
2023-06-16 01:42:33 -0500

    Updated urls for deployment

--- client/src/network.ts
+++ client/src/network.ts
@@ -1,6 +1,9 @@
-const shortenUrlApiCall = "http://localhost:3000/shorten/";
-const checkStoredUrlsApiCall = "http://localhost:3000/check/";
-const deleteUrlApiCall = "http://localhost:3000/delete";
+// const shortenUrlApiCall = "http://localhost:3000/shorten/";
+const shortenUrlApiCall = "https://us-rinp.onrender.com/";
+// const checkStoredUrlsApiCall = "http://localhost:3000/check/";
+const checkStoredUrlsApiCall = "https://us-rinp.onrender.com/check";
+// const deleteUrlApiCall = "http://localhost:3000/delete";
+const deleteUrlApiCall = "https://us-rinp.onrender.com/delete";
 
 export const apiCalls = {
     shorten: shortenUrlApiCall,

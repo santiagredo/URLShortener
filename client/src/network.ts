58b0c3ebde85d6cef5c91746c0c184b908de449c
Santiago S <santiagoagredo@live.com>
2023-06-16 01:54:25 -0500

    Corrected URLs for API calls

--- client/src/network.ts
+++ client/src/network.ts
@@ -1,9 +1,9 @@
 // const shortenUrlApiCall = "http://localhost:3000/shorten/";
-const shortenUrlApiCall = "https://us-rinp.onrender.com/";
+const shortenUrlApiCall = "https://us-rinp.onrender.com:3000/shorten";
 // const checkStoredUrlsApiCall = "http://localhost:3000/check/";
-const checkStoredUrlsApiCall = "https://us-rinp.onrender.com/check";
+const checkStoredUrlsApiCall = "https://us-rinp.onrender.com:3000/check";
 // const deleteUrlApiCall = "http://localhost:3000/delete";
-const deleteUrlApiCall = "https://us-rinp.onrender.com/delete";
+const deleteUrlApiCall = "https://us-rinp.onrender.com:3000/delete";
 
 export const apiCalls = {
     shorten: shortenUrlApiCall,

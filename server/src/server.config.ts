35a29fd5203019b41ec2d89bbd310da142bccbdf
Santiago S <santiagoagredo@live.com>
2023-06-16 02:04:36 -0500

    Correct server URLs

--- server/src/server.config.ts
+++ server/src/server.config.ts
@@ -1,6 +1,8 @@
 export const serverConfiguration = {
     PORT: 3000,
-    SERVER_LINK_ADDRESS: "http://localhost:3000/",
-    SERVER_LINK_ADDRESS_NOT_FOUND: "http://localhost:3000/404",
+    // SERVER_LINK_ADDRESS: "http://localhost:3000/",
+    SERVER_LINK_ADDRESS: "https://us-rinp.onrender.com/",
+    // SERVER_LINK_ADDRESS_NOT_FOUND: "http://localhost:3000/404",
+    SERVER_LINK_ADDRESS_NOT_FOUND: "https://us-rinp.onrender.com/404",
     AMOUNT_OF_CHARACTERS_TO_RANDOMIZE: 6
 };
